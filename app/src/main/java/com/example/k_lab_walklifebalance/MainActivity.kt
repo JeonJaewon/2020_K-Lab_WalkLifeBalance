13b00d681a4e593f4ded5cf67fa397e42e73285e
JeonJaewon <wksk04515@naver.com>
2020-06-04 04:17:01 +0900

    initial commit

---
+++ app/src/main/java/com/example/k_lab_walklifebalance/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.k_lab_walklifebalance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
