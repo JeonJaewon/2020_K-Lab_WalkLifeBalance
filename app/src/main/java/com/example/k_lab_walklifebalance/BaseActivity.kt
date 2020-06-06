4db5205fcc7c475598a88c59790e65aff759458c
JeonJaewon <wksk04515@naver.com>
2020-06-06 18:54:06 +0900

    Add bottom nav transition

--- app/src/main/java/com/example/k_lab_walklifebalance/BaseActivity.kt
+++ app/src/main/java/com/example/k_lab_walklifebalance/BaseActivity.kt
@@ -1,22 +1,17 @@
 package com.example.k_lab_walklifebalance
 
-import android.app.Activity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.android.synthetic.main.tool_bar.*
 
-
-open class BaseActivity : AppCompatActivity() {
-    lateinit var nav : BottomNavigationView
-
+// 툴바, 바텀네비게이션 사용하는 액티비티는 baseActivity를 상속받아서 구현
+open class BaseActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     // 상단 툴바 코드
@@ -39,5 +34,43 @@ open class BaseActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-
+    override fun onNavigationItemSelected(item : MenuItem): Boolean {
+        when(item.itemId){
+            R.id.statistics_menu ->{
+                val fm = supportFragmentManager
+                val trans = fm.beginTransaction()
+                // 동일한 이름의 클래스가 backstack에 있을시 삭제하는 구문
+                fm.popBackStack(this.javaClass.simpleName, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+                trans.replace(
+                    R.id.fragment_container,
+                    StatisticsFragment()
+                ).commit()
+                trans.addToBackStack(this.javaClass.simpleName)
+                return true
+            }
+            R.id.home_menu ->{
+                val fm = supportFragmentManager
+                val trans = fm.beginTransaction()
+                fm.popBackStack(this.javaClass.simpleName, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+                trans.replace(
+                    R.id.fragment_container,
+                    HomeFragment()
+                ).commit()
+                trans.addToBackStack(this.javaClass.simpleName)
+                return true
+            }
+            R.id.profile_menu ->{
+                val fm = supportFragmentManager
+                val trans = fm.beginTransaction()
+                fm.popBackStack(this.javaClass.simpleName, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+                trans.replace(
+                    R.id.fragment_container,
+                    ProfileFragment()
+                ).commit()
+                trans.addToBackStack(this.javaClass.simpleName)
+                return true
+            }
+            else->{ return false }
+        }
+    }
 }
--- app/src/main/java/com/example/k_lab_walklifebalance/MainActivity.kt
+++ app/src/main/java/com/example/k_lab_walklifebalance/MainActivity.kt
@@ -1,24 +1,27 @@
 package com.example.k_lab_walklifebalance
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.tool_bar.*
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(){
+    lateinit var  bottomNav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var toolbar = main_toolbar as Toolbar
         toolbar.title = ""
         setSupportActionBar(toolbar)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragment_container, HomeFragment())
+        transaction.commit()
+        bottomNav = main_bottom_nav as BottomNavigationView
+        bottomNav.setOnNavigationItemSelectedListener {
+            super.onNavigationItemSelected(it)
+        }
+        bottomNav.selectedItemId = R.id.home_menu
     }
+
 
 }
