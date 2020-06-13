6555909c1c8fec39c1bcc2ffe6d603d44777086e
JeonJaewon <wksk04515@naver.com>
2020-06-13 18:42:27 +0900

    Add new fragments and bottom navigation icon

--- app/src/main/java/com/example/k_lab_walklifebalance/HomeFragment.kt
+++ app/src/main/java/com/example/k_lab_walklifebalance/HomeFragment.kt
@@ -5,23 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 import kotlinx.android.synthetic.main.fragment_home.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -34,23 +21,49 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //기본 프래그먼트인 보폭으로 초기화
+        val trans = childFragmentManager.beginTransaction()
+        trans.add(R.id.home_fragment_container, StrideFragment())
+        trans.commit()
+
+        stride_btn.setOnClickListener {
+            val trans = childFragmentManager.beginTransaction()
+            trans.replace(
+                R.id.home_fragment_container,
+                StrideFragment()
+            )
+            trans.addToBackStack(null)
+            trans.commit()
+        }
+        balance_btn.setOnClickListener {
+            val trans = childFragmentManager.beginTransaction()
+            trans.replace(
+                R.id.home_fragment_container,
+                BalanceFragment()
+            )
+            trans.addToBackStack(null)
+            trans.commit()
+        }
+        angle_btn.setOnClickListener {
+            val trans = childFragmentManager.beginTransaction()
+            trans.replace(
+                R.id.home_fragment_container,
+                AngleFragment()
+            )
+            trans.addToBackStack(null)
+            trans.commit()
+        }
+        stand_btn.setOnClickListener {
+            val trans = childFragmentManager.beginTransaction()
+            trans.replace(
+                R.id.home_fragment_container,
+                StandFragment()
+            )
+            trans.addToBackStack(null)
+            trans.commit()
+        }
     }
+
 }
