870b679592638f37ebd65149277d49796464f44a
ske0318 <66875541+ske0318@users.noreply.github.com>
2020-06-15 19:11:32 +0900

    setting

--- app/src/main/java/com/example/k_lab_walklifebalance/SettingsActivity.kt
+++ app/src/main/java/com/example/k_lab_walklifebalance/SettingsActivity.kt
@@ -2,6 +2,9 @@ package com.example.k_lab_walklifebalance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.widget.Toolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_help.*
@@ -9,9 +12,11 @@ import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : BaseActivity() {
     lateinit var  bottomNav : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        init()
 
         var toolbar = settings_toolbar as Toolbar
         toolbar.title = ""
@@ -22,5 +27,36 @@ class SettingsActivity : BaseActivity() {
             super.onNavigationItemSelected(it)
         }
         // TODO(어떤 bootom nav 아이템이 선택되어 있게 할지)
+
+    }
+    fun init(){
+        var items = resources.getStringArray(R.array.textsize)
+        var myAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
+        spinner_textsize.adapter = myAdapter
+
+        spinner_textsize.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                when(position){
+                    0->{}
+                    1->{}
+                    2->{}
+                    3->{}
+                    4->{}
+                    5->{}
+                    6->{}
+                    7->{}
+                }
+            }
+
+        }
     }
 }
