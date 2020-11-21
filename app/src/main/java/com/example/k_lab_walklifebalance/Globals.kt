f7bd4c0105e7ae6d88152a11d5d0ecb245d07f20
JeonJaewon <wksk04515@naver.com>
2020-11-21 15:07:18 +0900

    Add setting variable to globals

--- app/src/main/java/com/example/k_lab_walklifebalance/Globals.kt
+++ app/src/main/java/com/example/k_lab_walklifebalance/Globals.kt
@@ -6,6 +6,10 @@ class Globals : Application() {
     private var gaitShape = 0
     private var isUserFall = false
     private val LoadPercentData = MutableList<Double>(3) { 0.0 }
+    private var soundEnabled = true
+    private var vibrationEnabled = true
+    private var notificationEnabled = true
+    private var biggerFontEnable = false
 
     fun setLoadData(Data:MutableList<Double>){
         for(i in 0..Data.size - 1){
@@ -19,7 +23,6 @@ class Globals : Application() {
     fun getGaitShape(): Int {
         return gaitShape
     }
-
     fun setGaitShape(value: Int) {
         gaitShape = value
     }
@@ -27,8 +30,35 @@ class Globals : Application() {
     fun getIsUserFall(): Boolean {
         return isUserFall
     }
-
     fun setIsUserFall(value: Boolean) {
         isUserFall = value
+    }
+
+    fun getSoundEnabled(): Boolean {
+        return soundEnabled
+    }
+    fun setSoundEnable(value: Boolean) {
+        soundEnabled = value
+    }
+
+    fun getVibrationEnabled(): Boolean {
+        return vibrationEnabled
+    }
+    fun setVibrationEnabled(value: Boolean) {
+        vibrationEnabled = value
+    }
+
+    fun getNotificationEnabled(): Boolean {
+        return notificationEnabled
+    }
+    fun setNotificationEnabled(value: Boolean) {
+        notificationEnabled = value
+    }
+
+    fun getBiggerFontEnable(): Boolean{
+        return biggerFontEnable
+    }
+    fun setBiggerFontEnable(value: Boolean){
+        biggerFontEnable = value
     }
 }
